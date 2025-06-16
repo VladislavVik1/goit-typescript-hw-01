05ed76691dc1d7b49339001a44fdef2abe04df29
VladislavVik1 <Dakraman1232@ukr.net>
2025-06-16 15:06:46 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/goit-typescript-hw-01/',
+  base: '/goit-typescript-hw-01/', // 👈 назва твого GitHub-репозиторію
 });
